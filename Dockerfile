ae29afd427a6ef92f78752520a08beed28ff5cfd
siddharthoak <sidoak@gmail.com>
2020-10-27 18:47:16 +0530

    change port to override parent

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,9 @@
 FROM sidoak/nodejs-parent:latest
+LABEL io.openshift.expose-services="8080:http"
+EXPOSE 8080
+
+USER 1001
+
+RUN chgrp -R 0 /usr/src/app && chown -R g=u /usr/src/app
 
 CMD [ "node", "child.js" ]
--- child.js
+++ child.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-const PORT = 80;
+const PORT = 8080;
 const HOST = '0.0.0.0';
 
 var server = http.createServer(function(request, response) {
@@ -9,6 +9,8 @@ var server = http.createServer(function(request, response) {
     response.end("Hello World from child and child.js!");
 
 });
+
+
 
 server.listen(PORT, HOST);
 
